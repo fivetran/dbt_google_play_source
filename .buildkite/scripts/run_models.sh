9243ee22059ecdfde349fd04fe603bbce2a9cc37
fivetran-joemarkiewicz <joseph.markiewicz@fivetran.com>
2022-10-26 09:07:28 -0500

    dbt-utils readme update

--- .buildkite/scripts/run_models.sh
+++ .buildkite/scripts/run_models.sh
@@ -17,7 +17,5 @@ dbt deps
 dbt seed --target "$db" --full-refresh
 dbt run --target "$db" --full-refresh
 dbt test --target "$db"
-dbt run --vars --vars '{google_play__using_earnings: true}' --target "$db" --full-refresh
-dbt test --target "$db"
 dbt run --vars --vars '{google_play__using_earnings: true, google_play__using_subscriptions: true}' --target "$db" --full-refresh
 dbt test --target "$db"
